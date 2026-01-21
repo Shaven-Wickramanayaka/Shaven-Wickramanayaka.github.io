65e0e47c48a7c34bc7ef210b2601004015cf2148
Shaven Dulneth Wickramanayaka <ShavenDulneth@gmail.com>
2026-01-21 12:43:27 +0000

    added other html files to build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import topLevelAwait from "vite-plugin-top-level-await";
 export default defineConfig({
   plugins: [topLevelAwait()],
   base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about.html',
+        contact: './contact.html',
+        projects: './projects.html',
+      }
+    }
+  }
 });
